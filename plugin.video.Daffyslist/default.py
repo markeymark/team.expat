9c222a91beb66c2eb4ea5dedc964ffa8409c3181
hmemar <husham.memar@husham.com>
2015-08-22 05:43:08 +0100

    Repo Update - Daffylist 1.0.1

--- plugin.video.Daffyslist/default.py
+++ plugin.video.Daffyslist/default.py
@@ -36,7 +36,7 @@ class NoRedirection(urllib2.HTTPErrorProcessor):
        
 CIBase = 'https://github.com/Daffy81/plugin.video.daffyslist-1.0/raw/master/daffyslist.xml'
 
-addon = xbmcaddon.Addon('plugin.video.revenge')
+addon = xbmcaddon.Addon('plugin.video.Daffyslist')
 addon_version = addon.getAddonInfo('version')
 profile = xbmc.translatePath(addon.getAddonInfo('profile').decode('utf-8'))
 home = xbmc.translatePath(addon.getAddonInfo('path').decode('utf-8'))
